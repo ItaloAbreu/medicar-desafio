e664e9e2be94751b7fc11d922568c7aed5bc01b0
Italo Abreu <italoabreu.m@gmail.com.br>
2021-09-23 23:23:03 -0300

    Concluido Interface administrativa

--- backend/consultas/admin.py
+++ backend/consultas/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
-from consultas.models import Agenda, Consulta
+from consultas.models import Agenda, HorarioAgenda 
 
-admin.site.register(Agenda)
-admin.site.register(Consulta)
+
+class HorarioAgendaInline(admin.StackedInline):
+    model = HorarioAgenda
+    fields = ('horario',)
+    extra = 1
+    min_num = 1
+
+
+@admin.register(Agenda)
+class AgendaAdmin(admin.ModelAdmin):
+    fields = ('medico', 'dia',)
+    inlines = [HorarioAgendaInline]
+
--- backend/medicar/urls.py
+++ backend/medicar/urls.py
@@ -19,3 +19,7 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+admin.site.index_title = 'Medicar'
+admin.site.site_header = 'Administração Medicar'
+admin.site.site_title = 'Administração Medicar'
