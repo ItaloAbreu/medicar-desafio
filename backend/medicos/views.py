78ec075bb6e9ff289a51eb7b465fd8c89b3ce17f
Italo Abreu <italoabreu.m@gmail.com.br>
2021-09-25 20:37:59 -0300

    Implentado search para especialidades

--- backend/medicos/views.py
+++ backend/medicos/views.py
@@ -1,18 +1,11 @@
-from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
-from rest_framework.response import Response
+from rest_framework import filters
 from medicos.serializers import EspecialidadeSerializer
 from medicos.models import Especialidade
 
 
-class EspecialidadeViewSet(viewsets.ViewSet):
-    def list(self, request):
-        queryset = Especialidade.objects.all()
-        serializer = EspecialidadeSerializer(queryset, many=True)
-        return Response(serializer.data)
-
-    def retrieve(self, request, pk=None):
-        queryset = Especialidade.objects.all()
-        user = get_object_or_404(queryset, pk=pk)
-        serializer = EspecialidadeSerializer(user)
-        return Response(serializer.data)
+class EspecialidadeViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Especialidade.objects.all()
+    serializer_class = EspecialidadeSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['nome']
