f7f7bb81c7ecc36a1caa404cf99962ad3580182a
Italo Abreu <italoabreu.m@gmail.com.br>
2021-09-25 21:38:13 -0300

    Filtro de especialidades aplicada a view de medico

--- backend/medicar/utils.py
+++ backend/medicar/utils.py
@@ -1,5 +1,28 @@
+from django.forms.fields import MultipleChoiceField
+from django_filters.filters import Filter
+
 SIGLA_ESTADOS = ('AC', 'AL', 'AP', 'AM', 'BA', 'CE',
                  'DF', 'ES', 'GO', 'MA', 'MT', 'MS',
                  'MG', 'PA', 'PB', 'PR', 'PE', 'PI',
                  'RJ', 'RN', 'RS', 'RO', 'RR', 'SC',
                  'SP', 'SE', 'TO',)
+
+
+class MultipleValueField(MultipleChoiceField):
+    def __init__(self, *args, field_class, **kwargs):
+        self.inner_field = field_class()
+        super().__init__(*args, **kwargs)
+
+    def valid_value(self, value):
+        return self.inner_field.validate(value)
+
+    def clean(self, values):
+        return values and [self.inner_field.clean(value) for value in values]
+
+
+class MultipleValueFilter(Filter):
+    field_class = MultipleValueField
+
+    def __init__(self, *args, field_class, **kwargs):
+        kwargs.setdefault('lookup_expr', 'in')
+        super().__init__(*args, field_class=field_class, **kwargs)
--- backend/medicos/views.py
+++ backend/medicos/views.py
@@ -1,5 +1,9 @@
 from rest_framework import viewsets
 from rest_framework import filters
+from django_filters import FilterSet
+from django_filters.rest_framework import DjangoFilterBackend
+from django.forms.fields import IntegerField
+from medicar.utils import MultipleValueFilter
 from medicos.serializers import EspecialidadeSerializer
 from medicos.serializers import MedicoSerializer
 from medicos.models import Especialidade
@@ -13,8 +17,17 @@ class EspecialidadeViewSet(viewsets.ReadOnlyModelViewSet):
     search_fields = ['nome']
 
 
+class MedicoFilterSet(FilterSet):
+    especialidade = MultipleValueFilter(field_class=IntegerField)
+
+    class Meta:
+        model = Medico
+        fields = ['especialidade']
+
+
 class MedicoViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Medico.objects.all()
     serializer_class = MedicoSerializer
-    filter_backends = [filters.SearchFilter]
+    filter_backends = [filters.SearchFilter, DjangoFilterBackend]
+    filter_class = MedicoFilterSet
     search_fields = ['nome']
