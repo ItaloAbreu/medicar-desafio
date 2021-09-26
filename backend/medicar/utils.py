7cc8ebf47f68194bce3a33899254c201c0656596
Italo Abreu <italoabreu.m@gmail.com.br>
2021-09-26 16:33:45 -0300

    utilizando IntegerField no campo CRM de Medico

--- backend/medicar/utils.py
+++ backend/medicar/utils.py
@@ -1,12 +1,6 @@
 from django.forms.fields import MultipleChoiceField
 from django_filters.filters import Filter
 
-SIGLA_ESTADOS = ('AC', 'AL', 'AP', 'AM', 'BA', 'CE',
-                 'DF', 'ES', 'GO', 'MA', 'MT', 'MS',
-                 'MG', 'PA', 'PB', 'PR', 'PE', 'PI',
-                 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC',
-                 'SP', 'SE', 'TO',)
-
 
 class MultipleValueField(MultipleChoiceField):
     def __init__(self, *args, field_class, **kwargs):
--- backend/medicos/models.py
+++ backend/medicos/models.py
@@ -1,9 +1,6 @@
-import re
 from django.db import models
-from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 from phonenumber_field.modelfields import PhoneNumberField
-from medicar.utils import SIGLA_ESTADOS
 
 
 class Especialidade(models.Model):
@@ -13,29 +10,9 @@ class Especialidade(models.Model):
         return self.nome
 
 
-def validate_crm(value: str) -> None:
-    """
-    CRM é formado somente por números seguido da Sigla do Estado
-    Ex: 0000/CE minímo 4 dígitos ou 0000000000/CE máximo 10 dígitos
-    """
-
-    regex = r'^[\d].{3,9}\/[a-zA-Z].{1}$'
-    if not re.match(regex, value):
-        raise ValidationError(
-            _("%(value)s não é um CRM válido. Utilize o formato '0000000000/CE'"),
-            params={'value': value},
-        )
-    sigla = value.split('/')[-1]
-    if not sigla.upper() in SIGLA_ESTADOS:
-        raise ValidationError(
-            _(f"{sigla} não corresponde a nenhuma sigla de estado brasileiro."),
-            params={'value': value},
-        )
-
-
 class Medico(models.Model):
     nome = models.CharField(max_length=240)
-    crm = models.CharField(max_length=13, validators=[validate_crm])
+    crm = models.IntegerField()
     email = models.EmailField(null=True, blank=True)
     telefone = PhoneNumberField(null=True, blank=True)
     especialidade = models.ForeignKey(Especialidade,
